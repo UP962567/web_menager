ee0c8ba3b3064d763a45af3063530f71a215cade
Maliq Dyrma <up962567@myport.ac.uk>
2024-05-05 10:56:47 +0100

    Y

--- src/components/(admin)/Header/menuData.tsx
+++ src/components/(admin)/Header/menuData.tsx
@@ -4,31 +4,7 @@ const menuData: Menu[] = [
   {
     id: 1,
     title: "Home",
-    path: "/",
-    newTab: false,
-  },
-  {
-    id: 2,
-    title: "About",
-    path: "/about",
-    newTab: false,
-  },
-  {
-    id: 3,
-    title: "Pricing",
-    path: "/pricing",
-    newTab: false,
-  },
-  {
-    id: 5,
-    title: "Contact",
-    path: "/contact",
-    newTab: false,
-  },
-  {
-    id: 5,
-    title: "Blog",
-    path: "/blogs",
+    path: "/dashobard",
     newTab: false,
   },
   {
@@ -40,36 +16,6 @@ const menuData: Menu[] = [
         id: 61,
         title: "About Page",
         path: "/about",
-        newTab: false,
-      },
-      {
-        id: 62,
-        title: "Pricing Page",
-        path: "/pricing",
-        newTab: false,
-      },
-      {
-        id: 63,
-        title: "Contact Page",
-        path: "/contact",
-        newTab: false,
-      },
-      {
-        id: 64,
-        title: "Blog Grid Page",
-        path: "/blogs",
-        newTab: false,
-      },
-      {
-        id: 66,
-        title: "Sign Up Page",
-        path: "/signup",
-        newTab: false,
-      },
-      {
-        id: 67,
-        title: "Sign In Page",
-        path: "/signin",
         newTab: false,
       },
       {
