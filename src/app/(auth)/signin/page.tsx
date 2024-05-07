0437080ae2187d339171e757f233e5bac7eb3d9f
Maliq Dyrma <up962567@myport.ac.uk>
2024-05-07 08:21:52 +0100

    Changes

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Signin from "@/components/Auth/SignIn";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import PreLoader from "@/components/Common/PreLoader";
@@ -5,11 +6,6 @@ import { Metadata } from "next";
 import { redirect } from "next/dist/server/api-utils";
 import { SessionProvider, useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation';
-
-export const metadata: Metadata = {
-  title:
-    "Sign In | Play SaaS Starter Kit and Boilerplate for Next.js",
-};
 
 const SigninPage = () => {
 
@@ -20,7 +16,7 @@ const SigninPage = () => {
     return <PreLoader />;
   } else if (status === "authenticated") {
     router.push("/");
-  }
+  } 
 
 
   return (
