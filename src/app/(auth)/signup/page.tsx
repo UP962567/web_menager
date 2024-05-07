b50ace6ccd352cc1fbed55ad6368814289b0f927
Maliq Dyrma <up962567@myport.ac.uk>
2024-05-07 08:23:05 +0100

    Changes

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,13 +1,21 @@
+"use client";
 import SignUp from "@/components/Auth/SignUp";
 import Breadcrumb from "@/components/Common/Breadcrumb";
+import PreLoader from "@/components/Common/PreLoader";
 import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title:
-    "Sign Up | Play SaaS Starter Kit and Boilerplate for Next.js",
-};
+import { useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation';
 
 const SignupPage = () => {
+  const { data: session, status } = useSession()
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <PreLoader />;
+  } else if (status === "authenticated") {
+    router.push("/");
+  } 
+
   return (
     <>
       <SignUp />
